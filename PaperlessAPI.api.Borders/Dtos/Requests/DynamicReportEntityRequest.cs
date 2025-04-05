00c4d08ed7cb70fa88c2c445b110d7aa1723b134
Valossa515 <fe.mmo515@gmail.com>
2025-04-05 19:57:35 -0300

    Ajustes de appsettings para mongo db atlas

--- PaperlessAPI.api.Borders/Dtos/Requests/DynamicReportEntityRequest.cs
+++ PaperlessAPI.api.Borders/Dtos/Requests/DynamicReportEntityRequest.cs
@@ -6,9 +6,21 @@
         IEnumerable<IEnumerable<string>> Rows,
         DateTime CreatedAt)
     {
+        /// <summary>
+        /// Titulo do relatório
+        /// </summary>
         public string Title { get; set; } = Title;
+        /// <summary>
+        /// Cabeçalhos do relatório
+        /// </summary>
         public IEnumerable<string> ColumnHeaders { get; set; } = ColumnHeaders;
+        /// <summary>
+        /// linhas do relatório
+        /// </summary>
         public IEnumerable<IEnumerable<string>> Rows { get; set; } = Rows;
+        /// <summary>
+        /// Data de criação do relatório
+        /// </summary>
         public DateTime CreatedAt { get; set; } = CreatedAt;
     }
 }
